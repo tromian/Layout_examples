d63cc8a73277a7e275eef3462f7f08466f4dc282
tromian <m22ttt@gmail.com>
2018-08-30 15:51:59 +0300

    commit in new branch

--- app/src/main/java/com/example/tromian/myapp1/MainActivity.java
+++ app/src/main/java/com/example/tromian/myapp1/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.tromian.myapp1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private Button button1;
+    private Button button2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        button2 = findViewById(R.id.button2);
+        textView = findViewById(R.id.textView);
+
+
     }
 }
